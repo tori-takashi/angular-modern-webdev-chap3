d22b7c1f0ac851d8c8e8ff2cefa41548eba1d650
tori-takashi <tori.toutoshi@gmail.com>
2018-08-10 16:58:51 +0900

    save

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
+import {MainComponent, ROUTES} from './main.component';
+import {Page01Component} from './page01.component';
+import {Page02Component} from './page02.component';
+import {CalcService} from './calc.service';
+import {StoreService} from './store.service';
+import {ChildComponent} from './child.component';
+import {CounterService} from './counter.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ //コンポーネント,ディレクティブ,パイプ
+    MainComponent,
+    Page01Component,
+    Page02Component,
+    ChildComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [  //外部@NgModuleで定義されたモジュール
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(ROUTES)
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ //DIするサービス
+    CalcService,
+    StoreService,
+    CounterService
+  ],
+  bootstrap: [MainComponent] //アプリ起動時コンポーネント
 })
-export class AppModule { }
+export class AppModule {
+}
